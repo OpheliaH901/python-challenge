b1ffc2b739127e32cf6896af0052748e4367b2d4
Ophelia Heard <opheliaheard@Ophelias-MBP.attlocal.net>
2021-05-06 18:14:01 -0500

    renamed file and modified main.py to include reader

--- PyBank/Main.py
+++ PyBank/Main.py
@@ -1,1 +1,21 @@
-Main.py
+# Modules
+import os
+import csv
+
+# Set path for file
+csvpath = "Resources/budget_data.csv"
+
+# with open(csvpath) as csvfile:
+
+#     # CSV reader specifies delimiter and variable that holds contents
+#     csvreader = csv.reader(csvfile, delimiter=',')
+
+#     print(csvreader)
+
+with open(csvpath) as csvfile:
+    csvreader = csv.reader(csvfile)
+    for row in csvreader:
+        print(row)
+
+    
+
