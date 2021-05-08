202c6bdfcce4b376d7a9c9a7ad841b3acc5913c1
Ophelia Heard <opheliaheard@Ophelias-MBP.attlocal.net>
2021-05-08 16:31:44 -0500

    cvsreader added

--- PyPoll/Main.py
+++ PyPoll/Main.py
@@ -1,1 +1,17 @@
+# Modules
+import os
+import csv
 
+# Set path for file
+csvpath = "Resources/election_data.csv"
+
+with open(csvpath) as csvfile:
+    csvreader = csv.reader(csvfile)
+    # for row in csvreader:
+    #     print(row)
+
+    # Read the header row first (skip this part if there is no header)
+    csv_header = next(csvreader)
+
+     #Read each row. Header not included
+    for row in csvreader:
