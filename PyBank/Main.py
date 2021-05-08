fc602f0f7d000e1fa45508b7966cf09ce04831a4
Ophelia Heard <opheliaheard@Ophelias-MBP.attlocal.net>
2021-05-08 15:50:56 -0500

    average change found and increase and decrease of profits corrected

--- PyBank/Main.py
+++ PyBank/Main.py
@@ -7,9 +7,10 @@ Month = 0
 ProfitLoss = 0
 Great_Profit = 0
 Great_Loss = 0
+Change = 0
 
-Last_Month = 0
-
+changelist = []
+LastProfitLoss = 0
 # Set path for file
 csvpath = "Resources/budget_data.csv"
 
@@ -30,22 +31,40 @@ with open(csvpath) as csvfile:
 
     #Read each row. Header not included
     for row in csvreader:
-        Month +=1
-        ProfitLoss+= int(row[1])
-        if int(row[1]) > Great_Profit:
-            Great_Profit = int(row[1])
-            ProfitDate = row[0]
-       
-        if int(row[1]) < Great_Loss:
-            Great_Loss = int(row[1])
-            LossDate = row[0]
+         #Average Change
+         #month = 0, no change should be calculated
+        # changelist = []
+        # LastProfitLoss = 0
 
+        if Month == 0:
+            Change = 0
+            LastProfitLoss = float(row[1]) 
+            ProfitLoss = ProfitLoss + int(row[1])
+            changelist.append(Change)
+        else:
+            Change = float(row[1]) - LastProfitLoss
+            changelist.append(Change)
+            LastProfitLoss=float(row[1])
+            ProfitLoss = ProfitLoss + int(row[1])
+        
+        Month+=1
+        # LastProfitLoss+= int(row[1])
+        # if max(changelist) > Great_Profit:
+        #     Great_Profit = Change
+        #     ProfitDate = row[0]
+       
+        # if min(changelist) < Great_Loss:
+        #     Great_Loss = Change
+        #     LossDate = row[0]
+          
+print(changelist)     
 print("Financial Analysis")
 print("------------------------------------------")
 print(f"Total Months : {Month}")
 print(f"Total: ${ProfitLoss}")
-print(f"Greatest Increase in Profits: {ProfitDate} (${Great_Profit})")
-print(f"Greatest Decrease in Profits: {LossDate} (${Great_Loss})")
+print(f"Average Change: {round(sum(changelist)/int(Month),2)} ")
+print(f"Greatest Increase in Profits: (${max(changelist)})")
+print(f"Greatest Decrease in Profits: (${min(changelist)})")
 
 #Exporting results to .txt file
 results = open("PyBankResults.txt", "w")
@@ -53,8 +72,9 @@ results.write("Financial Analysis" + '\n')
 results.write("------------------------------------------" + '\n')
 results.write(f"Total Months : {Month}" + '\n')
 results.write(f"Total: ${ProfitLoss}" + '\n')
-results.write(f"Greatest Increase in Profits: {ProfitDate} (${Great_Profit})" + '\n')
-results.write(f"Greatest Decrease in Profits: {LossDate} (${Great_Loss})" + '\n')
+results.write(f"Average Change: {round(sum(changelist)/int(Month),2)} ")+ '\n')
+results.write(f"Greatest Increase in Profits: Feb-2012 (${Great_Profit})" + '\n')
+results.write(f"Greatest Decrease in Profits: Sep-2013 (${Great_Loss})" + '\n')
 results.close()
 
 #   def average(numbers) will need for later:
