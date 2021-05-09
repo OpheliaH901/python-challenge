c78dcb47daa5b9f139e1b4acb061ca7bb43bd66f
Ophelia Heard <opheliaheard@Ophelias-MBP.attlocal.net>
2021-05-08 23:25:56 -0500

    importing text file and print statements

--- PyPoll/Main.py
+++ PyPoll/Main.py
@@ -1,6 +1,20 @@
 # Modules
 import os
 import csv
+import collections
+from collections import Counter
+
+
+
+#Set my variables
+Votes_Count = 0
+Total = 0
+VoterID = 0
+
+
+# A list of actors
+Candidate_list = [
+    "Correy" , "Khan", "Li", "O'Tooley"]
 
 # Set path for file
 csvpath = "Resources/election_data.csv"
@@ -13,5 +27,31 @@ with open(csvpath) as csvfile:
     # Read the header row first (skip this part if there is no header)
     csv_header = next(csvreader)
 
-     #Read each row. Header not included
+
+  #Read each row. Header not included. Loop counter through each row to give me total amount of votes
+  
     for row in csvreader:
+ #pull from collections module and assign so that total can be provided when we are reay to loop through data
+        if list==row[2]:
+            Votes_Count = Counter(list)  
+            print(Votes_Count)
+
+
+    
+        
+
+
+print("Election Results")
+print("------------------------------------------")
+print("------------------------------------------")
+print("------------------------------------------")
+print("------------------------------------------")
+
+#Exporting results to .txt file
+results = open("ElectionDataResults.txt", "w")
+results.write("Election Results" + '\n')
+results.write("------------------------------------------" + '\n')
+results.write(f"Total Votes : {Votes_Count}" + '\n')
+results.write("------------------------------------------" + '\n')
+
+results.close()
